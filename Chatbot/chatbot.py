92c45cf0c5f29d68b32a4e5732c0fb6ff4637ebf
YatoMoose <s.y.topchiy@student.khai.edu>
2021-11-11 08:25:28 +0200

    ChatBot 4-th stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -9,3 +9,19 @@ remainder5 = int(input())
 remainder7 = int(input())
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print ("Your age is " + str(age) + " that's a good time to start programming!")
+print ("Now I will prove to you that I can count to any number you want.")
+number = int(input())
+for i in range(number + 1):
+    print(i, '!', sep='')
+
+print ("Let's test your programming knowledge.")
+print ("Why do we use methods?")
+print ("1. To repeat a statement multiple times.")
+print ("2. To decompose a program into several small subroutines.")
+print ("3. To determine the execution time of a program.")
+print ("4. To interrupt the execution of a program.")
+x = 0
+while x != 2:
+    x = int(input())
+    if x == 2:
+        print ("Completed, have a nice day!")
