8a70d786c38a5473d647f55a15bf456131ef57ff
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-07 22:02:08 +0200

    Hangman 3-rd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,9 +1,14 @@
+import random
+
 print("""HANGMAN
 The game will be available soon.""")
 
-word = "python"
+listWords=["python", "java", "javascript", "php"]
+
+selectWord = random.choice(listWords)
 writeWord = input("Guess the word: > ")
-if writeWord == word :
+
+if writeWord == selectWord:
     print("You survived!")
 else :
     print("You lost!")
