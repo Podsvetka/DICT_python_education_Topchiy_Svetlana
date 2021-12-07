beadc72d089256096743b65bb3e0c35cdc60475f
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-07 22:34:16 +0200

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -6,7 +6,15 @@ The game will be available soon.""")
 listWords=["python", "java", "javascript", "php"]
 
 selectWord = random.choice(listWords)
-writeWord = input("Guess the word: > ")
+
+help = ''
+for i in range(len(selectWord)):
+    if i < 3:
+        help += selectWord[i]
+    else:
+        help +="-"
+
+writeWord = input("Guess the word: "+help+"> ")
 
 if writeWord == selectWord:
     print("You survived!")
