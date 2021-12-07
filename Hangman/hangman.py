8202c2a08a9a0c81c1fc519cd7779063c0abb1ab
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-08 00:04:13 +0200

    Hangman 8-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,47 +4,56 @@ print("""HANGMAN
 The game will be available soon.""")
 
 listWords=["python", "java", "javascript", "php"]
-guesses = 8
 
-selectWord = random.choice(listWords)
-listLetter = set(selectWord)
-listUseLetter = set()
+while True :
+    print('Type "play" to play the game, "exit" to quit:')
+    enter = input("> ")
 
-help = list("-")*len(selectWord)
-while True:
-    print("".join(help))
-    writeLetter = input("Input a letter: > ")
+    if enter == "play":
 
-    if len(writeLetter) > 1 or len(writeLetter) == 0:
-        print("You should input a single letter ")
-        continue
-    elif writeLetter.isupper() :
-        print('Please enter a lowercase English letter')
-        continue
+        guesses = 8
 
-    if writeLetter in listLetter :
-        listLetter.remove(writeLetter)
+        selectWord = random.choice(listWords)
+        listLetter = set(selectWord)
+        listUseLetter = set()
 
-        for i in range(len(selectWord)) :
-            if selectWord[i] == writeLetter :
-                help[i] = writeLetter
-    else :
+        help = list("-")*len(selectWord)
+        while True:
+            print("".join(help))
+            writeLetter = input("Input a letter: > ")
 
-        if writeLetter in listUseLetter:
-            print("You've already guessed this letter.")
-        else:
-            print("That letter doesn't appear in the word")
-            guesses -= 1
+            if len(writeLetter) > 1 or len(writeLetter) == 0:
+                print("You should input a single letter ")
+                continue
+            elif writeLetter.isupper() :
+                print('Please enter a lowercase English letter')
+                continue
 
-    if writeLetter in selectWord :
-        listUseLetter.add(writeLetter)
+            if writeLetter in listLetter :
+                listLetter.remove(writeLetter)
 
-    if len(listLetter) == 0 :
-        print("You survived!")
+                for i in range(len(selectWord)) :
+                    if selectWord[i] == writeLetter :
+                        help[i] = writeLetter
+            else :
+
+                if writeLetter in listUseLetter:
+                    print("You've already guessed this letter.")
+                else:
+                    print("That letter doesn't appear in the word")
+                    guesses -= 1
+
+            if writeLetter in selectWord :
+                listUseLetter.add(writeLetter)
+
+            if len(listLetter) == 0 :
+                print("You survived!")
+                break
+            elif guesses==0:
+                print("You lost!")
+                break
+
+        print("Thanks for playing!\nWe'll see how well you did in the next stage")
+
+    elif enter == "exit":
         break
-    elif guesses==0:
-        print("You lost!")
-        break
-
-print('''Thanks for playing!
-We'll see how well you did in the next stage''')
