ecb0a4dbdca7a67eecf6ce49a667af253d49c9e9
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-07 21:51:14 +0200

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,9 @@
 print("""HANGMAN
 The game will be available soon.""")
+
+word = "python"
+writeWord = input("Guess the word: > ")
+if writeWord == word :
+    print("You survived!")
+else :
+    print("You lost!")
