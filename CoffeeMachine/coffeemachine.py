fa392449135f2f161e7c202613a1e8cffa84d838
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-08 19:48:51 +0200

    CoffeeMachine 4-th stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -4,31 +4,83 @@ Boiling water
 Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
-Coffee is ready!""")
+Coffee is ready!
+""")
+
+availableWater = 400
+availableMilk = 540
+availableBeans = 15
+availableMoney = 550
+availableCup = 9
 
 
+def printStatus():
+    print("""The coffee machine has:
+    {} of water
+    {} of milk
+    {} of coffee beans
+    {} of disposable cups
+    {} of money""".format(availableWater, availableMilk, availableBeans, availableCup, availableMoney))
 
-print( "Write how many ml of water the coffee machine has:")
-availableWater = int(input(">"))
-print("Write how many ml of milk the coffee machine has:")
-availableMilk = int(input(">"))
-print("Write how many grams of coffee beans the coffee machine has:")
-availableBeans = int(input(">"))
+printStatus()
 
-maxCoffee = min(availableWater//200, availableMilk//50, availableBeans//15)
 
-print("Write how many cups of coffee you will need:")
-cups = int(input(">"))
+print("Write action (buy, fill, take):")
+action = input(">")
 
-water =  200*cups
-milk = 50*cups
-beans =  15*cups
+if action == "buy":
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    typeCoffee = int(input(">"))
 
-if water <= availableWater and milk <= availableMilk and beans <= availableBeans:
+    maxCoffee = min(availableWater // 200, availableMilk // 50, availableBeans // 15)
 
-    if maxCoffee == 1:
-        print("Yes, I can make that amount of coffee")
-    elif maxCoffee > cups:
-        print("Yes, I can make that amount of coffee (and even {} more than that)".format(maxCoffee-cups))
-else:
-    print("No, I can make only {} cups of coffee".format(maxCoffee))
+    water = milk = beans = price = 0
+
+    if typeCoffee == 1:
+        water = 250
+        milk = 0
+        beans = 16
+        price = 4
+    elif typeCoffee == 2:
+        water = 350
+        milk = 75
+        beans = 20
+        price = 7
+    elif typeCoffee == 3:
+        water = 200
+        milk = 100
+        beans = 12
+        price = 6
+
+    if water <= availableWater and milk <= availableMilk and beans <= availableBeans:
+
+        if maxCoffee == 1:
+            print("Yes, I can make that amount of coffee")
+        elif maxCoffee > 1:
+            print("Yes, I can make that amount of coffee (and even {} more than that)".format(maxCoffee - 1))
+
+        availableMoney += price
+        printStatus()
+
+    else:
+        print("No, I can make only {} cups of coffee".format(maxCoffee))
+
+elif action =="fill":
+    print("Write how many ml of water you want to add:")
+    addWater = int(input(">"))
+    print("Write how many ml of milk you want to add:")
+    addMilk = int(input(">"))
+    print("Write how many grams of coffee beans you want to add:")
+    addBeans = int(input(">"))
+    print("Write how many disposable coffee cups you want to add:")
+    addCups = int(input(">"))
+
+    availableWater += addWater
+    availableMilk += addMilk
+    availableBeans += addBeans
+    availableCup += addCups
+    printStatus()
+elif action == "take":
+    print("I gave you {}".format(availableMoney))
+    availableMoney = 0
+    printStatus()
