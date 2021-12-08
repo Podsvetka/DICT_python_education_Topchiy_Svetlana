09abe4f57e4dad5ed9f7f8cf7afa725ffc625aed
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-08 19:05:34 +0200

    CoffeeMachine 3-rd stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -5,10 +5,30 @@ Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
+
+
+
+print( "Write how many ml of water the coffee machine has:")
+availableWater = int(input(">"))
+print("Write how many ml of milk the coffee machine has:")
+availableMilk = int(input(">"))
+print("Write how many grams of coffee beans the coffee machine has:")
+availableBeans = int(input(">"))
+
+maxCoffee = min(availableWater//200, availableMilk//50, availableBeans//15)
+
 print("Write how many cups of coffee you will need:")
 cups = int(input(">"))
 
-print("""For {} cups of coffee you will need:
-{} ml of water
-{} ml of milk
-{} g of coffee beans""".format(cups, 200*cups, 50*cups, 15*cups))
+water =  200*cups
+milk = 50*cups
+beans =  15*cups
+
+if water <= availableWater and milk <= availableMilk and beans <= availableBeans:
+
+    if maxCoffee == 1:
+        print("Yes, I can make that amount of coffee")
+    elif maxCoffee > cups:
+        print("Yes, I can make that amount of coffee (and even {} more than that)".format(maxCoffee-cups))
+else:
+    print("No, I can make only {} cups of coffee".format(maxCoffee))
