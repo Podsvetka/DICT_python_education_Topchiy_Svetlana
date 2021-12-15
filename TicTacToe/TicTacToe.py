edc3f803138bea7da3157393b571112e0430740c
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-15 20:40:24 +0200

    TicTacToe 4-th stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,21 +1,22 @@
 fill = input("Enter cells:")
 
-print ("-"*5)
-for y in range(3):
-    print("|", end="")
-    for x in range(3):
-        print(fill[y*3+x],end="")
-    print("|")
-print ("-"*5)
+def field():
+    print("-" * 5)
+    for y in range(3):
+        print("|", end="")
+        for x in range(3):
+            print(fill[y * 3 + x], end="")
+        print("|")
+    print("-" * 5)
+
 
 def win():
-
     win_side = ""
 
     for y in range(3):
         line = ""
         for x in range(3):
-            line += fill[y*3+x]
+            line += fill[y * 3 + x]
         if line.count(line[0]) == 3:
             win_side += line[0]
 
@@ -28,7 +29,7 @@ def win():
             line += fill[y * 3 + x]
             if x == y:
                 diag1 += fill[y * 3 + x]
-            if x == 2-y:
+            if x == 2 - y:
                 diag2 += fill[y * 3 + x]
 
         if line.count(line[0]) == 3:
@@ -37,19 +38,45 @@ def win():
             win_side += diag1[0]
         if diag2.count(diag2[0]) == 3:
             win_side += diag2[0]
-    return win_side
+    return win_side.replace("_", "")
 
-if abs(fill.count("X")- fill.count("O")) > 1 or len(win())>1:
+
+
+field()
+
+if abs(fill.count("X") - fill.count("O")) > 1 or len(win()) > 1:
     print("Impossible")
 else:
     if "_" in fill:
-       if len(win()) == 1:
-           print(win() + " wins")
-       else :
-           print("Game not finished")
+        if len(win()) == 1:
+            print(win() + " wins")
+        else:
+            # print("Game not finished")
+
+            while True:
+
+                coord = input("Enter the coordinates:").strip().split(" ")
+                if len(coord) == 2 and coord[0].isnumeric() and coord[1].isnumeric():
+                    x = int(coord[0]) - 1
+                    y = int(coord[1]) - 1
+                    if 0 <= x <= 2 and 0 <= y <= 2:
+                        if fill[y * 3 + x] == "_":
+                            listEl=list(fill)
+                            listEl[y * 3 + x] = "X"
+                            fill= "".join(listEl)
+                            field()
+                            break
+                        else:
+                            print("This cell is occupied! Choose another one!")
+                    else:
+                        print("Coordinates should be from 1 to 3!")
+                else:
+                    print("You should enter numbers!")
+
+
 
     else:
         if len(win()) == 1:
             print(win() + " wins")
-        else :
+        else:
             print("Draw")
