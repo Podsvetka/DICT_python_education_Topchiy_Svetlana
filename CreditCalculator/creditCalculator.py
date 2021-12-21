4fed7244c5905acd751acf03ffd74b4fce894566
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-21 23:09:23 +0200

    creditCalculator 4-th stage

--- CreditCalculator/creditCalculator.py
+++ CreditCalculator/creditCalculator.py
@@ -1,70 +1,104 @@
 import math
-print("What do you want to calculate?")
-print('type "a" for annuity monthly payment amount,')
-print('type "p" for loan principal:')
-print('type "n" for number of monthly payments,')
-
-type = input(">")
-
-if type == "n":
-    print("Enter the loan principal:")
-    principal = int(input(">"))
-    print("Enter the monthly payment:")
-    payment = int(input(">"))
-    print("Enter the loan interest:")
-    percent = int(input(">"))/100.0
-    i = percent/(12*1)
-
-    n = math.log(payment/(payment-i * principal), 1.0+i)
-    n = math.ceil(n)
-    year = n/12
-    if year %1 > 0 :
-        month = (year%1)*12
-
-        year = int(year)
-        month = int(month)
-
-        if month == 0:
-            print(f"It will take {year} years to repay this loan!")
-        elif year == 0:
-            print(f"It will take {month} months to repay this loan!")
-        else:
-            print(f"It will take {year} years and {month} months to repay this loan!")
-
-    else:
-        print(f"It will take {year} years to repay this loan!")
+import sys
 
 
-
-elif type =="a":
-    print("Enter the loan principal:")
-    principal = float(input(">"))
-    print("Enter the number of periods:")
-    period = int(input(">"))
-    print("Enter the loan interest:")
-    percent = float(input(">"))/100
-    i = percent/(12*1)
-    payment = (i * principal*math.pow(i+1, period)/ (math.pow(i+1, period)-1))
-    payment = math.ceil(payment)
-    print(f"Your monthly payment = {int(payment)}!")
+def getArgValue(nameArg):
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i].startswith("--" + nameArg + "="):
+            arg = sys.argv[i]
+            return arg[arg.index("=") + 1:len(arg)]
+    return None
 
 
-elif type == "p":
-    print("Enter the annuity payment:")
-    payment = float(input(">"))
-    print("Enter the number of periods:")
-    period = int(input(">"))
-    print("Enter the loan interest:")
-    percent = float(input(">"))/100
-    i = percent/(12*1)
-    principal = - (payment - payment*math.pow(i+1, period))/(i*math.pow(i+1, period))
-    principal = math.floor(principal)
-    print(f"Your loan principal = {int(principal)}!")
+def isNumber(n):
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
 
-    # payment = principal/period
-    # if payment %1 > 0:
-    #     payment = math.ceil(payment)
-    #     lastpayment = principal-(period-1)*payment
-    #     print(f"Your monthly payment = {payment} and the last payment = {lastpayment}.")
-    # else:
-    #     print(f"Your monthly payment = {payment}")
+
+def error():
+    print("Incorrect parameters")
+    exit(1)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        error()
+    type = getArgValue("type")
+    if not type in ["annuity", "diff"]:
+        error()
+    payment = getArgValue("payment")
+    if payment and (not isNumber(payment) or int(payment) <= 0):
+        error()
+    if payment:
+        payment = float(payment)
+
+    principal = getArgValue("principal")
+    if principal and (not isNumber(principal) or int(principal) <= 0):
+        error()
+    if principal:
+        principal = float(principal)
+
+    periods = getArgValue("periods")
+    if periods and (not isNumber(periods) or int(periods) <= 0):
+        error()
+    if periods:
+        periods = int(periods)
+
+    interest = getArgValue("interest")
+    if not periods or not isNumber(interest) or float(interest) <= 0:
+        error()
+    interest = float(interest) / 100
+
+    if type == "diff":
+        if not principal or not periods:
+            error()
+
+        i = interest / (12 * 1)
+        sum = 0
+        for m in range(1, periods + 1):
+            diff = principal / periods + i * (principal - (principal * (m - 1)) / periods)
+            diff = math.ceil(diff)
+            print(f"Month {m}: payment is {diff}")
+            sum += diff
+        print(f"Overpayment = {int(sum - principal)}")
+
+    elif type == "annuity":
+        if principal and payment:
+            i = interest / (12 * 1)
+
+            n = math.log(payment / (payment - i * principal), 1.0 + i)
+            n = math.ceil(n)
+            year = n / 12
+            if year % 1 > 0:
+                month = (year % 1) * 12
+
+                year = int(year)
+                month = int(month)
+
+                if month == 0:
+                    print(f"It will take {year} years to repay this loan!")
+                elif year == 0:
+                    print(f"It will take {month} months to repay this loan!")
+                else:
+                    print(f"It will take {year} years and {month} months to repay this loan!")
+
+            else:
+                print(f"It will take {year} years to repay this loan!")
+            print(f"Overpayment = {int((n * payment) - principal)}")
+
+        elif principal and periods:
+            i = interest / (12 * 1)
+            payment = (i * principal * math.pow(i + 1, periods) / (math.pow(i + 1, periods) - 1))
+            payment = math.ceil(payment)
+            print(f"Your monthly payment = {int(payment)}!")
+            print(f"Overpayment = {int((periods * payment) - principal)}")
+
+        elif payment and periods:
+            i = interest / (12 * 1)
+            principal = - (payment - payment * math.pow(i + 1, periods)) / (i * math.pow(i + 1, periods))
+            principal = math.floor(principal)
+            print(f"Your loan principal = {int(principal)}!")
+            print(f"Overpayment = {int((periods * payment) - principal)}")
