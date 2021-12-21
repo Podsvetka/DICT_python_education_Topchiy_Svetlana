6f8dec36915797ac6554e464841bd71ef609300c
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-21 21:08:51 +0200

    creditCalculator 3-rd stage

--- CreditCalculator/creditCalculator.py
+++ CreditCalculator/creditCalculator.py
@@ -1,23 +1,70 @@
 import math
-print("Enter the loan principal:")
-principal = int(input(">"))
 print("What do you want to calculate?")
-print('type "m" – for number of monthly payments,')
-print('type "p" – for the monthly payment:')
+print('type "a" for annuity monthly payment amount,')
+print('type "p" for loan principal:')
+print('type "n" for number of monthly payments,')
+
 type = input(">")
 
-if type == "m":
+if type == "n":
+    print("Enter the loan principal:")
+    principal = int(input(">"))
     print("Enter the monthly payment:")
     payment = int(input(">"))
-    countMonth = math.floor(principal/payment)
-    print(f"It will take {countMonth} month to repay the loan")
-elif type =="p":
-    print("Enter the number of months:")
-    period = int(input(">"))
-    payment = principal/period
-    if payment %1 > 0:
-        payment = math.ceil(payment)
-        lastpayment = principal-(period-1)*payment
-        print(f"Your monthly payment = {payment} and the last payment = {lastpayment}.")
+    print("Enter the loan interest:")
+    percent = int(input(">"))/100.0
+    i = percent/(12*1)
+
+    n = math.log(payment/(payment-i * principal), 1.0+i)
+    n = math.ceil(n)
+    year = n/12
+    if year %1 > 0 :
+        month = (year%1)*12
+
+        year = int(year)
+        month = int(month)
+
+        if month == 0:
+            print(f"It will take {year} years to repay this loan!")
+        elif year == 0:
+            print(f"It will take {month} months to repay this loan!")
+        else:
+            print(f"It will take {year} years and {month} months to repay this loan!")
+
     else:
-        print(f"Your monthly payment = {payment}")
+        print(f"It will take {year} years to repay this loan!")
+
+
+
+elif type =="a":
+    print("Enter the loan principal:")
+    principal = float(input(">"))
+    print("Enter the number of periods:")
+    period = int(input(">"))
+    print("Enter the loan interest:")
+    percent = float(input(">"))/100
+    i = percent/(12*1)
+    payment = (i * principal*math.pow(i+1, period)/ (math.pow(i+1, period)-1))
+    payment = math.ceil(payment)
+    print(f"Your monthly payment = {int(payment)}!")
+
+
+elif type == "p":
+    print("Enter the annuity payment:")
+    payment = float(input(">"))
+    print("Enter the number of periods:")
+    period = int(input(">"))
+    print("Enter the loan interest:")
+    percent = float(input(">"))/100
+    i = percent/(12*1)
+    principal = - (payment - payment*math.pow(i+1, period))/(i*math.pow(i+1, period))
+    principal = math.floor(principal)
+    print(f"Your loan principal = {int(principal)}!")
+
+    # payment = principal/period
+    # if payment %1 > 0:
+    #     payment = math.ceil(payment)
+    #     lastpayment = principal-(period-1)*payment
+    #     print(f"Your monthly payment = {payment} and the last payment = {lastpayment}.")
+    # else:
+    #     print(f"Your monthly payment = {payment}")
