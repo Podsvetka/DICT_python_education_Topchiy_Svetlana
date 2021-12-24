ee5a2fb9cb7127a936a80a84264d831a4592f3c9
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-24 21:07:47 +0200

    Dominoes 4-th stage

--- Dominoes/dominoes.py
+++ Dominoes/dominoes.py
@@ -9,6 +9,7 @@ def dominoes():
         player = domino_pieces[21:28]
         computer = domino_pieces[14:21]
         tier_list = []
+    while True:
         for i in player:
             if i == i[::-1]:
                 tier_list.append(i)
@@ -25,50 +26,76 @@ def dominoes():
         m = tier_list
     if m in player:
         player.remove(m)
-    elif m in computer:
+        choice = "Status: Computer is about to make a move. Press Enter to continue..."
+    else:
         computer.remove(m)
-    print(f"""Stock pieces {len(stock)}
-    Computer pieces {len(computer)}
-    {m}
-    Your pieces:
-        """)
-    for i, item in enumerate(player):
-        print(f"{i + 1}: {item}")
-    print("")
-    if computer > player:
-        print("Status: Computer is about to make a move. Press Enter to continue...")
-    elif computer < player:
-        input("Status: It's your turn to make a move. Enter your command. ")
+        choice = "Status: It's your turn to make a move. Enter your command. "
 
 
-def interface():
+    tier_list = [m]
+
+def interface(m):
     print("=" * 70)
     print(f"""Stock pieces {len(stock)}
     Computer pieces {len(computer)}
-    {m} 
+    If you don't have a suitable dominance press 0 and you get 1 domino.
      """)
-    print("""Your pieces:
+    if len(m) <= 6:
+        print(m)
+    elif len(m) > 6:
+        print(f"""{m[:3]}...{m[-3:]}""")
+    print("""
+    Your pieces:
            """)
     for x, item in enumerate(player):
         print(f"{x + 1}: {item}")
 
+def check():
+    while True:
+        player_answer = input("Status: It's your turn to make a move. Enter your command. ")
+        try:
+            int(player_answer)
+            if int(player_answer) not in range(-len(player), len(player) + 1):
+                print("you don't have that much")
+                continue
+            else:
+                return player_answer
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue
 
-def player_input(player_answer):
+def player_input():
     turn = False
+    player_answer = int(check())
     while not turn:
         if player_answer < 0:
-            tier_list.insert(0, player[-player_answer - 1])
-            player.remove(player[-player_answer - 1])
-            turn = True
+            if player[abs(player_answer) - 1][-1] == tier_list[0][0]:
+                tier_list.insert(0, player[-player_answer - 1])
+                player.remove(player[-player_answer - 1])
+                turn = True
+            elif player[abs(player_answer) - 1][0] == tier_list[0][0]:
+                tier_list.insert(0, player[-player_answer - 1][::-1])
+                player.remove(player[-player_answer - 1])
+                turn = True
+
         elif player_answer > 0:
-            tier_list.append(player[player_answer - 1])
-            player.remove(player[player_answer - 1])
-            turn = True
+            if player[player_answer - 1][0] == tier_list[-1][1]:
+                tier_list.append(player[player_answer - 1])
+                player.remove(player[player_answer - 1])
+                turn = True
+            elif player[player_answer - 1][1] == tier_list[-1][1]:
+                tier_list.append(player[player_answer - 1][::-1])
+                player.remove(player[player_answer - 1])
+                turn = True
+            else:
+                print("wrong one")
+                player_answer = int(check())
+
+
         elif player_answer == 0:
             player.append(stock[0])
             del stock[0]
             turn = True
-
         else:
             print("Incorrect input. Enter number ")
             continue
@@ -77,56 +104,52 @@ def player_input(player_answer):
 def computer_input():
     turn = False
     while not turn:
-        computer_answer = random.choice(range(-len(computer), len(computer)+1))
+        computer_answer = random.choice(range(-len(computer), len(computer) + 1))
         if computer_answer < 0:
-            tier_list.insert(0, computer[-computer_answer - 1])
-            computer.remove(computer[-computer_answer - 1])
-            turn = True
+            if computer[abs(computer_answer) - 1][-1] == tier_list[0][0]:
+                tier_list.insert(0, computer[abs(computer_answer) - 1])
+                computer.remove(computer[abs(computer_answer) - 1])
+                turn = True
+            elif computer[abs(computer_answer) - 1][0] == tier_list[0][0]:
+                tier_list.insert(0, computer[abs(computer_answer) - 1][::-1])
+                computer.remove(computer[abs(computer_answer) - 1])
+                turn = True
         elif computer_answer > 0:
-            tier_list.append((computer[computer_answer - 1]))
-            computer.remove(computer[computer_answer - 1])
-            turn = True
-        elif computer_answer == 0:
+            if computer[computer_answer - 1][0] == tier_list[-1][1]:
+                tier_list.append(computer[computer_answer - 1])
+                computer.remove(computer[computer_answer - 1])
+                turn = True
+            elif computer[computer_answer - 1][1] == tier_list[-1][1]:
+                tier_list.append(computer[computer_answer - 1][::-1])
+                computer.remove(computer[computer_answer - 1])
+                turn = True
+        else:
             computer.append(stock[0])
             del stock[0]
             turn = True
 
 
-interface(tier_list)
 
-win = False
-while not win:
+while True:
     if len(computer) > 0 and len(player) > 0:
         if choice == "Status: Computer is about to make a move. Press Enter to continue...":
             print(input("Status: Computer is about to make a move. Press Enter to continue..."))
             computer_input()
-            choice = "It's your turn to make a move. Enter your command"
+            choice = "Status: It's your turn to make a move. Enter your command"
         else:
-            move = input("Status: It's your turn to make a move. Enter your command. ")
-            try:
-                int(move)
-            except ValueError:
-                print("Invalid input. Please try again.")
-                continue
-            else:
-                if int(move) not in range(-len(player), len(player) + 1):
-                    print("you don't have that much")
-                    continue
-                if int(move) in range(-len(computer), len(computer)+1):
-                    player_input(int(move))
-                    choice = "Status: Computer is about to make a move. Press Enter to continue..."
-                else:
-                    print("Invalid input. Please try again.")
-                    continue
-    interface(tier_list)
-    if len(player) == 0:
-        print("""The game is over. You won!
-        You have 0 pieces""")
-        break
-    elif len(computer) == 0:
-        print("The game is over. The computer won!")
-        break
-    elif tier_list[0][0] == tier_list[-1][-1]:
+            player_input()
+            choice = "Status: Computer is about to make a move. Press Enter to continue..."
+
+
+interface(tier_list)
+if len(player) == 0:
+    print("The game is over. You won!")
+    print("You have 0 pieces")
+    exit
+elif len(computer) == 0:
+    print("The game is over. The computer won!")
+    exit
+elif tier_list[0][0] == tier_list[-1][-1]:
         score = 0
         for i in range(len(tier_list)):
             for s in tier_list[i]:
@@ -134,4 +157,11 @@ while not win:
                     score += 1
         if score == 8:
             print("The game is over. It's a draw!")
-            break
+            exit
+elif len(stock) == 0:
+        if choice == "Status: Computer is about to make a move. Press Enter to continue...":
+                print("The game is over. You won!")
+                exit
+        elif choice == "Status: It's your turn to make a move. Enter your command":
+                print("The game is over. The computer won!")
+                exit
