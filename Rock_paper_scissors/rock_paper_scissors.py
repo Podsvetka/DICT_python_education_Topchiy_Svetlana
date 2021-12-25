ab2a4d2075069a544ed9703086522b9f59332bd2
YatoMoose <s.y.topchiy@student.khai.edu>
2021-12-25 22:05:02 +0200

    Rock Paper Scissors 4-th stage

--- Rock_paper_scissors/rock_paper_scissors.py
+++ Rock_paper_scissors/rock_paper_scissors.py
@@ -1,16 +1,30 @@
 import random
+
+name = str(input("Enter your name:"))
+print(f"Hello: {name}")
+point = 0
+r = open('rating.txt', 'r')
+for context in r:
+    nick, score = context.split()
+    if name == nick:
+        point = int(score)
+
 while True:
     x = {"scissors": "paper", "paper": "rock", "rock": "scissors"}
     player = str(input("Enter your choice:"))
     computer = random.choice([*x.keys()])
+    if player == "rating":
+        print(f"your score: {point}")
     if player == computer:
         print("Draw")
+        point += 50
         continue
     if player == x.get(computer):
         print(f"Sorry, but the computer chose {computer}")
         continue
     if x.get(player) == computer:
         print(f"Well done. The computer chose {computer} and failed")
+        point += 100
         continue
     if player == "exit":
         print("Bye")
