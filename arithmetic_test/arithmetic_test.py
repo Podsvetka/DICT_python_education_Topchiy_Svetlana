896351054836a6f9f294f1bc30ac1ccb18f5662b
YatoMoose <s.y.topchiy@student.khai.edu>
2022-02-22 18:37:37 +0200

    Arithmetic test 2-nd stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,23 +1,41 @@
 import random
 
 
-a = random.randrange(2,9)
-oper = random.choice(["+","-","*"])
-b = random.randrange(2,9)
-answer = 0
+countMark = 5
+rightMark = 0
 
-if oper == "+":
-    answer = a+b
-elif oper == "-":
-    answer = a-b
-elif oper == "*":
-    answer = a*b
 
-print(f"{a} {oper} {b}")
-answerUser = input(">")
+for n in range(countMark):
 
-if int(answerUser) == answer:
-    print("Right!")
-else:
-    print("Wrong!")
+
+
+    a = random.randrange(2,9)
+    oper = random.choice(["+","-","*"])
+    b = random.randrange(2,9)
+    answer = 0
+
+    if oper == "+":
+        answer = a+b
+    elif oper == "-":
+        answer = a-b
+    elif oper == "*":
+        answer = a*b
+
+    print(f"{a} {oper} {b}")
+    answerUser = 0
+    while(True):
+        try:
+            answerUser = int(input(">"))
+            break
+        except:
+            print("Incorrect format")
+
+
+    if int(answerUser) == answer:
+        print("Right!")
+        rightMark += 1
+    else:
+        print("Wrong!")
+
+print(f"Your mark is {rightMark}/{countMark}")
 
